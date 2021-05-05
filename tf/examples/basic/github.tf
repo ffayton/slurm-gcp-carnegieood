51b31c523facf9ef741602518cccd5b720ee717e
Floyd Fayton <ffayton@gmail.com>
2021-05-05 12:19:11 -0700

    new

--- tf/examples/basic/github.tf
+++
@@ -1,8 +0,0 @@
-provider "github" {
-    token = "e368ba11c975fc90fa4c456cbf08cd6734d5c72d"
-}
-
-resource "github_repository" "slurm-gcp-carnegieood" {
-  name        = "slurm-gcp-carnegieood"
-  description = "SLURM GCP repo for use with Terraform"
-}
